76f3477695ba993af4893cc6f9c03cd8b39824ba
Полина Полянская <ppolina353@yandex.ru>
2024-04-28 15:19:35 +0300

    first commit

---
+++ functions.py
@@ -0,0 +1,14 @@
+def gener_list_BKGx(lengthBKG):
+    pass
+    #return list_BKGx
+
+def preobr_list_BKGx_to_intx(list_BKGx):
+    pass 
+    #return list_intx
+
+def preobr_list_intx_to_realx(list_intx):
+    pass
+    #return list_realx
+    
+def grafik_realx(realx1, realx2):
+    pass
